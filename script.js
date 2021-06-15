ccd633be40471e7c6f8679291d2d871dd614a087
SilverCloud-1 <84994532+SilverCloud-1@users.noreply.github.com>
2021-06-15 13:03:46 +0300

    Update script.js
    File is completely rewritten.

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+//Check for positive values
+function validateAmount() {
+    var principal = document.getElementById("principal").value;
+    var moreOrEqualZero = parseInt(principal) >= 0;
+    if (!moreOrEqualZero) {
+        alert("Error. Enter a positive number please");
+        document.getElementById("principal").focus();
+    }
+}
+
+//Update the rate value
+function getSliderValue() {
+    document.getElementById("rateSpan").innerHTML = document.getElementById("rate").value;
+}
+
+function compute() {
+//Get the values and create
+var principal = parseFloat(document.getElementById("principal").value);
+var rate = parseFloat(document.getElementById("rate").value);
+var years = parseInt(document.getElementById("years").value);
+var interest = principal * years * rate /100;
+var yearInTheFuture = new Date().getFullYear()+parseInt(years);
+   
+document.getElementById("result").innerHTML = "If you deposit <mark>" + principal + "</mark>,<br/>" +
+        "at an interest rate of <mark>" + rate + "%"+"</mark>,<br/>" +
+        "You will receive an amount of <mark>" + interest + "</mark>,<br/> " +
+        "in the year <mark>" + yearInTheFuture + "</mark>";
 }
         
